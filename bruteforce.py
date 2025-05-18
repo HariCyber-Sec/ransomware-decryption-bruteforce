5e56cb046e868b300b5388943ec6bfb0353310d5
Harikumar Nandakumar <harikumarnandakumar@gmail.com>
2025-05-19 00:33:34 +0400

    Update bruteforce.py
    updated

--- bruteforce.py
+++ bruteforce.py
@@ -1,1 +1,40 @@
+'''
+Forage AIG Cybersecurity Program
+Bruteforce starter template
+'''
 
+from zipfile import ZipFile
+# Use a method to attempt to extract the zip file with a given password
+# def attempt_extract(zf_handle, password):
+def attempt_extract(zf_handle, password):
+    try:
+        # Try to extract all files using the password (must be bytes)
+        zf_handle.extractall(pwd=password)
+        return True
+    except:
+        return False
+
+
+
+def main():
+    print("[+] Beginning bruteforce ")
+    with ZipFile('enc.zip') as zf:
+        with open('rockyou.txt', 'rb') as f:
+            for line in f:
+                password = line.strip()  # Remove newline characters
+                if attempt_extract(zf, password):
+                    print(f"[+] Password found: {password.decode('utf-8')}")
+                    return
+    print("[-] Password not found in list")
+            # Write your logic here...
+            # Iterate through password entries in rockyou.txt
+
+            # Attempt to extract the zip file using each password
+
+            # Handle correct password extract versus incorrect password attempt)
+
+    #print("[+] Password not found in list")
+
+if __name__ == "__main__":
+    main()
+     
